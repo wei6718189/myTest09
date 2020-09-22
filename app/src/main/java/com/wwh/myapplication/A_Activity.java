34110f9cdbb77abf811950c83ff6730909dd5968
weiweihua <597609230@qq.com>
2020-09-22 17:41:01 +0800

    提交 修改aaa

--- app/src/main/java/com/wwh/myapplication/A_Activity.java
+++ app/src/main/java/com/wwh/myapplication/A_Activity.java
@@ -3,12 +3,16 @@ package com.wwh.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class A_Activity extends AppCompatActivity {
+
+    private static final String TAG = "111";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_a_);
+        Log.d(TAG, "log Aaaa");
+
     }
 }
