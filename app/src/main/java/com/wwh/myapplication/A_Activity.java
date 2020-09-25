30f23a85fa8df2d5427701103a6243a399230722
weiweihua <597609230@qq.com>
2020-09-25 11:55:13 +0800

    提交dev2 a_activy 9

--- app/src/main/java/com/wwh/myapplication/A_Activity.java
+++ app/src/main/java/com/wwh/myapplication/A_Activity.java
@@ -28,6 +28,7 @@ public class A_Activity extends AppCompatActivity {
         Log.d(TAG, "log new dev2 6");
         Log.d(TAG, "log new dev2 7");
         Log.d(TAG, "log new dev2 8");
+        Log.d(TAG, "log new dev2 9");
 
     }
 }
